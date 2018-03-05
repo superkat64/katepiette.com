b6eb97ec7d915942a5cd80a2b423ea36e69212bf
Kate Piette <kat.piette@gmail.com>
2018-03-04 22:52:54 -0500

    Add Yarn & update scrollTo

--- js/scrollto-custom.js
+++ js/scrollto-custom.js
@@ -1,10 +1,12 @@
-$(document).ready(function(){
-  /**
-   * This part causes smooth scrolling using scrollto.js
-   * We target all a tags inside the nav, and apply the scrollto.js to it.
-   */
-  $(".pg-link").click(function(evn){
-      evn.preventDefault();
-      $('html body').scrollTo(this.hash, this.hash, {offset:100});
+$(document).ready(function() {
+  // Bind to the click of all links with a #hash in the href
+  $('a[href^="#"]').click(function(e) {
+    // Prevent the jump and the #hash from appearing on the address bar
+    e.preventDefault();
+    // Scroll the window, stop any previous animation, stop on user manual scroll
+    // Check https://github.com/flesler/jquery.scrollTo for more customizability
+    $(window)
+      .stop(true)
+      .scrollTo(this.hash, {duration: 1000, interrupt: true});
   });
 });
